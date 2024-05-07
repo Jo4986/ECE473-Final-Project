4ddda79cfa6954e4145f656185948bcdd05c4a0d
Jo4986 <144515790+Jo4986@users.noreply.github.com>
2024-05-06 20:16:46 -0400

    Update DAOToken.sol

--- DAOToken.sol
+++ DAOToken.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
+
+// This code introduces a custom role `CCUS_ROLE` for a specific company, allowing it to lock and unlock tokens
+// owned by token holders, ensuring controlled token transfers according to specified permissions.
+// Functions that  grant and revoke the `CCUS_ROLE` role, enhancing flexibility in token management, are also provided.
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -12,21 +17,21 @@ contract DAOToken is ERC20, AccessControl {
     bytes32 public constant CCUS_ROLE = keccak256("CCUS_ROLE");
     mapping(address => uint256) private _lockedTokens;
 
-    constructor(string memory name, string memory symbol, uint256 initialSupply)
-        ERC20(name, symbol) {
+    constructor(string memory name, string memory sign, uint256 initialSupply)
+        ERC20(name, sign) {
         _mint(msg.sender, initialSupply);
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     function lockToken(address account, uint256 amount) public onlyRole(CCUS_ROLE) {
-        require(amount <= balanceOf(account), "DAOToken: cannot lock more than the balance");
+        require(amount <= balanceOf(account), "DAOToken: You are not permitted to lock more than the balance");
         _lockedTokens[account] = _lockedTokens[account].add(amount);
         _transfer(account, address(this), amount);
     }
 
     function unlockToken(address account, uint256 amount) public onlyRole(CCUS_ROLE) {
         uint256 lockedBalance = _lockedTokens[account];
-        require(amount <= lockedBalance, "DAOToken: cannot unlock more tokens than are locked");
+        require(amount <= lockedBalance, "DAOToken: You are not permitted to unlock more tokens than are locked");
         _lockedTokens[account] = _lockedTokens[account].sub(amount);
         _transfer(address(this), account, amount);
     }
